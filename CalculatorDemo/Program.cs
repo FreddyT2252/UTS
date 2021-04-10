538a648e0778f1b6ae0e8bcec5bbba6422f105d5
FreddyT2252 <79196216+FreddyT2252@users.noreply.github.com>
2021-04-10 09:35:10 +0700

    Add files via upload
    Freddy Tanusina
    20/456841/TK/50665
    Ini merupakan calcultaor biasa, yang bida digunakan untuk membantuk perhitungan di bisnis bisnid

---
+++ CalculatorDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CalculateDemo
+{
+    class Program
+    {
+        /// <summary>
+        /// Dictionary of the record method
+        /// </summary>
+        static Dictionary<string, string> cfg = new Dictionary<string, string>();
+        static void Main(string[] args)
+        {
+            Load();
+            while(true)
+            {
+                Console.Write("Please enter operand 1:");
+                int x = int.Parse(Console.ReadLine());
+                Console.Write("Please enter operand 2:");
+                int y = int.Parse(Console.ReadLine());
+                Console.Write("Please enter the operator:");
+                string op = Console.ReadLine();
+                Operator opr = OperatorFactory.Create(cfg[op]);
+                opr.Left = x;
+                opr.Right = y;
+                int r = opr.Calc();
+                Console.WriteLine($"{x}{op}{y}={r}");
+            }
+        }
+        /// <summary>
+        /// Initialization assignment
+        /// </summary>
+        static void Load()
+        {
+            cfg.Add("+", "CalculateDemo.AddOperator");
+            cfg.Add("-","CalculateDemo.SubOperator");
+            cfg.Add("*", "CalculateDemo.MulOperator");
+            cfg.Add("/", "CalculateDemo.DivOperator");
+        }
+    }
+}
